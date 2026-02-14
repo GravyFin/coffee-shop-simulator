c0ee0b704529da8757677e34fe7c1cf0ab2a88e2
GravyFin <146840692+GravyFin@users.noreply.github.com>
2026-02-14 01:24:41 +0000

    Updated feedback with more options
    Added more feedback options regarding results at the end of the day.

--- scripts/results_menu.gd
+++ scripts/results_menu.gd
@@ -6,25 +6,26 @@ extends Control
 @onready var popularity_label = $ResultsPanel/MarginContainer/VBoxContainer/Popularity
 @onready var staff_happiness_label = $ResultsPanel/MarginContainer/VBoxContainer/StaffHappiness
 @onready var feedback_label = $ResultsPanel/MarginContainer/VBoxContainer/Feedback
+@onready var tip_label = $ResultsPanel/MarginContainer/VBoxContainer/TipLabel
 
 func update_shop_stats():
 	# Advertising -> Popularity
 	if GameManager.advertising_spend == 0:
-		GameManager.popularity -= 20
+		GameManager.popularity -= 15
 	else:
-		GameManager.popularity += int(GameManager.advertising_spend / 10.0)
+		GameManager.popularity += int(GameManager.advertising_spend / 25.0)
 	
 	# Equipment -> Coffee Quality
 	if GameManager.equipment_spend == 0:
-		GameManager.coffee_quality -= 20
+		GameManager.coffee_quality -= 15
 	else: 
-		GameManager.coffee_quality += int(GameManager.equipment_spend / 10.0)
+		GameManager.coffee_quality += int(GameManager.equipment_spend / 25.0)
 	
 	# Staff -> Staff Happiness
 	if GameManager.staff_spend == 0:
-		GameManager.staff_happiness -= 20
+		GameManager.staff_happiness -= 15
 	else:
-		GameManager.staff_happiness += int(GameManager.staff_spend / 10.0)
+		GameManager.staff_happiness += int(GameManager.staff_spend / 25.0)
 	
 	clamp_stats()
 
@@ -59,6 +60,7 @@ func _ready():
 	
 	update_ui(customers, revenue)
 	update_feedback()
+	update_tip()
 
 func update_ui(customers: int, revenue: int):
 	customers_label.text = "Customers today: %d" % customers
@@ -69,22 +71,70 @@ func update_ui(customers: int, revenue: int):
 
 func update_feedback():
 	var messages := []
-	if GameManager.advertising_spend > 0:
-		messages.append("Well done! Your advertising brought in more customers!! :D")
-	else:
-		messages.append("Not a lot of people knew about your shop! :(")
 	
-	if GameManager.equipment_spend > 0:
-		messages.append("Well done! Your coffee tasted really good today! :D")
+	var ad = GameManager.advertising_spend
+	if ad == 0:
+		messages.append("Whoops! Not a lot of people knew about your shop! :(")
+	elif ad <= 100:
+		messages.append("Okay.. your adverts helped a little bit, but there weren't that many!")
+	elif ad <= 250:
+		messages.append("You sold adverts well today, you got more customers than usual!")
 	else:
-		messages.append("Uh oh! Some customers didn't like your coffee! :(")
+		messages.append("Well done! Your adverts brought in a lot of customers! :D")
 	
-	if GameManager.staff_spend > 0:
-		messages.append("Well done! Your staff were super happy and worked very hard! :D")
+	var eq = GameManager.equipment_spend
+	if eq == 0:
+		messages.append("Uh oh! Your coffee didn't really taste that good today! :(")
+	elif eq <= 100:
+		messages.append("You made some upgrades and the coffee was a little bit better!")
+	elif eq <= 250:
+		messages.append("Your coffee was pretty good today thanks to the new machines!")
 	else:
+		messages.append("Well done! Your coffee tasted super yummy today! :D")
+	
+	var sf = GameManager.staff_spend
+	if sf == 0:
 		messages.append("Whoops! Your staff felt tired and sad today! :(")
+	elif sf <= 100:
+		messages.append("Your staff worked a little bit harder today!")
+	elif sf <= 250:
+		messages.append("Your staff worked were happy and hard-working today!")
+	else:
+		messages.append("You staff were super happy today, they worked super hard! :D")
 	
 	feedback_label.text = "\n".join(messages)
+
+func generate_tip():
+	var ad = GameManager.advertising_spend
+	var eq = GameManager.equipment_spend
+	var sf = GameManager.staff_spend
+	
+	# === ADVERTISING TIPS ===
+	if ad == 0:
+		tip_label.text = "Try spending some more money on advertising so people know about you coffee shop!"
+		return
+	elif ad <= 100:
+		tip_label.text = "A little bit more advertising would bring in even more customers into to your coffee shop!"
+		return
+	
+	# === EQUIPMENT TIPS ===
+	if eq == 0:
+		tip_label.text = "Try to spend more money on your coffee shop equipment so you coffee tastes even better!"
+		return
+	elif eq <= 100:
+		tip_label.text = "A little more money into your coffee equipment would make your coffee really yummy!"
+		return
+	
+	# === STAFF TIPS ===
+	if sf == 0:
+		tip_label.text = "Try spending more money on you staff, they will be happier and work way harder for you!"
+		return
+	elif sf <= 100:
+		tip_label.text = "Your staff would work way harder if you paid them a little bit more money!"
+		return
+	
+	# === POSITIVE FEEDBACK ===
+	tip_label.text = "Great job! Your coffee shop is running really well! Keep up the good work!"
 
 func _on_next_day_pressed():
 	get_tree().change_scene_to_file("res://scenes/allocation_menu.tscn")
